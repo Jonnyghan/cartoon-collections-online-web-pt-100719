6f041a6e175b0ad8ff7abd0d5f079d28e42c68cf
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-21 01:00:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def summon_captain_planet(array)
   i = 1
   while i < array.length do
   a.push(array.collect { |n| n + "!"})
-  b.push(a.collect { |n| n.capitalize})
+  b.push(a.collect { |n| n.capitalize })
   i += 1
   b
   
